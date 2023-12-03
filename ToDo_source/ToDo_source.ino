f4e76117f8f3eecba5b9aa70ccfed5610df896cb
goulgoul <antoine.gln571@gmail.com>
2023-12-03 17:20:38 +0100

    Remade the whole code

--- ToDo_source/ToDo_source.ino
+++ ToDo_source/ToDo_source.ino
@@ -1,9 +1,8 @@
 #include "defines.hpp"
 #include "fonctions.hpp"
-uint8_t pins_fin_activite[] = {4, 5, 6};
+uint8_t pins_fin_activite[] = { 4, 5, 6 };
 
-typedef enum
-{
+typedef enum {
   DEBUT_JOURNEE,
   ACTIVITE_EN_ATTENTE,
   ACTIVITE_EN_COURS,
@@ -12,85 +11,67 @@ typedef enum
   JOURNEE_TERMINEE
 } etats_possibles_t;
 
-etats_possibles_t etat_actuel;
-uint8_t numero_activite = 0;
+etats_possibles_t etat_actuel = DEBUT_JOURNEE;
+uint8_t numero_activite = 1;
 uint32_t timer_activite = 0;
+bool INTERRUPTION_ACTIVITE_TERMINEE = false;
+
+
+void isr_fin_activite_1()
+{
+  INTERRUPTION_ACTIVITE_TERMINEE = true;
+}
+
 
 void setup() 
 {
   Serial.begin(BAUD_RATE);
-  pinMode(BOUTON_JAUGE, INPUT_PULLDOWN)
+  delay(1000);
+  pinMode(BOUTON_JAUGE, INPUT_PULLDOWN);
+  attachInterrupt(digitalPinToInterrupt(PIN_FIN_ACTIVITE_1), isr_fin_activite_1, CHANGE);
+  Serial.println("Init done");
 }
 
 void loop() 
 {
-  switch(etat_actuel)
+  switch (etat_actuel) 
   {
-    case DEBUT_JOURNEE: //init
+    case DEBUT_JOURNEE:
+      Serial.println("Debut journée");
       etat_actuel = ACTIVITE_EN_ATTENTE;
-      Serial.println("Début de la journée");
       break;
 
     case ACTIVITE_EN_ATTENTE:
-      // clignoter jauge
       Serial.println("Activité en attente");
-
-      while (!digitalRead(BOUTON_JAUGE))
-      {
-        // clignoter_jauge(BLANC);
-      }
+      while (!digitalRead(BOUTON_JAUGE));
       etat_actuel = ACTIVITE_EN_COURS;
       break;
-
+  
     case ACTIVITE_EN_COURS:
-      Serial.println("Activité en cours");
-
-      while (!digitalRead(BOUTON_JAUGE))
+      Serial.println("Début de l'activité");
+      while (!digitalRead(BOUTON_JAUGE) && !INTERRUPTION_ACTIVITE_TERMINEE)
       {
-        if (digitalRead(pins_fin_activite[numero_activite]))
-        {
-          etat_actuel = ACTIVITE_TERMINEE;
-          break;
-        }
         timer_activite++;
-        delay(1000);
+        delay(10);
       }
-      etat_actuel = ACTIVITE_EN_PAUSE;
-      
+      timer_activite /= 100;
+      Serial.println("Timer activité n° " + String(numero_activite) + " : " + String(timer_activite) + " secondes.");
+      etat_actuel = ACTIVITE_TERMINEE;
       break;
-      
-    case ACTIVITE_EN_PAUSE:
-      Serial.println("Activité en pause");
-
-      // FAIRE CLIGNOTER LA JAUGE
-      while(!digitalRead(BOUTON_JAUGE))
-      {
-        // clignoter_jauge(ORANGE);
-      }
-      etat_actuel = ACTIVITE_EN_COURS;
-      
-      break;
-
+  
     case ACTIVITE_TERMINEE:
-      Serial.println("Activité terminée");
-
-      actualiser_jauge();
-      if (numero_activite == NOMBRE_ACTIVITES)
-      {
-        etat_actuel = JOURNEE_TERMINEE;
+        numero_activite >= NOMBRE_ACTIVITES ? etat_actuel = JOURNEE_TERMINEE : etat_actuel = ACTIVITE_EN_ATTENTE;
+        numero_activite++;
         break;
-      }
-      numero_activite++;
-      break;
-
+    
     case JOURNEE_TERMINEE:
       Serial.println("Journée terminée");
-
+      etat_actuel = DEBUT_JOURNEE;
       break;
 
     default:
-      Serial.println("Défaut");
-
+      Serial.println("Default case");
+      etat_actuel = DEBUT_JOURNEE;
       break;
   }
   delay(1000);
